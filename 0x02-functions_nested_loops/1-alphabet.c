7e263d370769211453f0352b54460fe9dc00eb37
ODUFUWA ODUKUNLE ANTHONY <odufuwaodukunle2015@gmail.com>
2023-08-15 13:17:01 -0700

    0x02 functions nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,16 @@
 #include "main.h"
 /**
- * main - Entry point
- * print_alphabet - Prints the alphabets in lowercase.
- * Return: always 0
+ * print_alphabet - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
-        int i;
+	int i;
 
-        for (i = 97; i <= 122; i++)
-        {
-                _putchar(i);
-        }
-        _putchar('\n');
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n);
 }
-
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
- * print_alaphabet_x10 - Prints the alphabet 10 times.
+ * Description: 'print_alphabet_x10 - Prints the alphabet 10 times'
  *
- * Return: Always 0 (Succes)
+ * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
 {
-        int i;
-        int j;
+	int i;
+	int j;
 
-        for (j = 1; i <= 10; i++)
-        {
-                for (j = 97; j <= 122; j++)
-                {
-                        _putchar(j);
-                }
-                _putchar('\n');
-        }
+	for (j = 1; i <= 10; i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n')
+	}
 }
