a31f633d7a99850adfb40cd59cb6ac45d7fc9986
ODUFUWA ODUKUNLE ANTHONY <odufuwaodukunle2015@gmail.com>
2023-08-15 11:30:26 -0700

    0x02 functions nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include "main.h"
 /**
- * main - Entry point
- * Description: 'print _putchar to stdout'
- * Return: always 0
+ * main - print _putchar
+ * is written by kunle
+ * Return: 0
  */
+
 int main(void)
 {
-         _putchar('_');
-         _putchar(p_');
-         _putchar('p');
-         _putchar('t');
-         _putchar('c');
-         _putchar('h');
-         _putchar('a');
-         _putchar('r');
-         _putchar('\n');
-         return (0);
-}        
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
